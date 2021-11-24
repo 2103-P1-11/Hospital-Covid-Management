fac8e960b29bbb04fe16b5fbdfc3aaf4578b319a
angelenejoshna <angelenejoshna@gmail.com>
2021-11-24 18:53:46 +0800

    hospital page

--- src/main.js
+++ src/main.js
@@ -6,8 +6,16 @@ import router from './router'
 import cors from 'cors'
 import './theme/main.css'
 import store from './vuex/store'
+import * as VueGoogleMaps from 'vue2-google-maps'
 
 Vue.config.productionTip = false
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyB3MQBkbulZvgJZvqOi2gQxvNV377SqgcI',
+    libraries: 'places', // This is required if you use the Autocomplete plugin
+  },
+})
 
 // createApp(App).use(router).use(VueRouter).use(vuetify).use(cors).mount('#app')
 new Vue({
