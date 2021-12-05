428439d2dc2de7308bb8fa03a2eac631165e4851
SageSG <houliangyip@gmail.com>
2021-12-06 00:06:18 +0800

    advance features img

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -4,20 +4,40 @@ import Vuex from 'vuex';
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
-    state(){
-      return {
-        page:'Dashboard'
-        }
-    },
-    mutations: {
-      renamePage (state, rename){
-        state.page = rename
-      },
-      
-    },
-    actions: {
-      
+  state() {
+    return {
+      page: 'Dashboard',
+      patientName: '',
+      patientID: '',
+      hospitalID: '',
+      wardID: '',
     }
-  });
+  },
+  mutations: {
+    renamePage(state, rename) {
+      state.page = rename
+    },
+    renamePatient(state, n) {
+      state.patientName -= n
+    },
+    renameID(state, n) {
+      state.patientID -= n
+    },
+    renameHospital(state, n) {
+      state.hospitalID -= n
+    },
+    renameWard(state, n) {
+      state.wardID -= n
+    },
+
+  },
+  actions: {
+    changeUser({ commit }, name) {
+      setTimeout(() => {
+        commit('userName', name)
+      }, 1000)
+    },
+  }
+});
 
 export default store;
